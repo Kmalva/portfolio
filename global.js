54c2b424471f75ad4800ad625f058491c81fe046
Kmalva <alvarezkatem@gmail.com>
2026-04-17 20:02:24 -0700

    add js component

--- global.js
+++ global.js
@@ -3,3 +3,78 @@ console.log('IT’S ALIVE!');
 function $$(selector, context = document) {
   return Array.from(context.querySelectorAll(selector));
 }
+
+let pages = [
+  { url: '', title: 'Home' },
+  { url: 'projects/', title: 'Projects' },
+  { url: 'resume/', title: 'Resume' },
+  { url: 'contact/', title: 'Contact' },
+  { url: 'https://github.com/kmalva', title: 'GitHub' }
+];
+
+let nav = document.createElement('nav');
+document.body.prepend(nav);
+
+const BASE_PATH =
+  location.hostname === "localhost" || location.hostname === "127.0.0.1"
+    ? "/"
+    : "/website/";
+
+for (let p of pages) {
+  let url = p.url;
+  let title = p.title;
+
+  
+  url = !url.startsWith('http') ? BASE_PATH + url : url;
+
+  let a = document.createElement('a');
+  a.href = url;
+  a.textContent = title;
+
+
+  if (a.host === location.host && a.pathname === location.pathname) {
+    a.classList.add('current');
+  }
+
+  if (url.startsWith('http')) {
+    a.target = '_blank';
+    a.rel = 'noopener noreferrer';
+  }
+
+  nav.append(a);
+}
+document.body.insertAdjacentHTML(
+  'afterbegin',
+  `
+  <label class="color-scheme">
+    Theme:
+    <select>
+      <option value="auto">Automatic</option>
+      <option value="light">Light</option>
+      <option value="dark">Dark</option>
+    </select>
+  </label>`
+);
+
+let select = document.querySelector('.color-scheme select');
+
+select.addEventListener('input', function (event) {
+  console.log('color scheme changed to', event.target.value);
+
+  localStorage.colorScheme = event.target.value;
+
+  document.documentElement.style.setProperty(
+    'color-scheme',
+    event.target.value
+  );
+});
+
+
+if ("colorScheme" in localStorage) {
+  document.documentElement.style.setProperty(
+    'color-scheme',
+    localStorage.colorScheme
+  );
+
+  select.value = localStorage.colorScheme;
+}
